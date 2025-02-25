b3f7a463ae8099f9306413d46b31f0aed87cdbf6
AnastasiaYakovleva04 <nastasiakovleva@gmail.com>
2025-02-25 21:09:40 +0500

    v12.kt

--- v12.kt
+++ v12.kt
@@ -1,18 +1,46 @@
 fun main() {
     try {
-            val move: Cinema = Cinema()
-            print("Название: ")
-            move.name = readln()
-           print("Сеанс: ")
-            move.session = readln()
-            print("Залл: ")
-            move.hall = readln().toInt()
-            print("Цена: ")
-            move.price = readln().toInt()
-            print("Количество зврителей: ")
-            move.spectators = readln().toInt()
-            println(move.Info(move))
-            println("Выручка с сеанса = ${move.Revenue(move.price, move.spectators)}")
+        print("Сколько фильмов вы хотите внести: ")
+        val n = readln().toInt()
+        when
+        {
+            n <= 0 -> println("Неверно введено количество фильмов")
+            else -> {
+                val moves = Array(n) { Cinema() }
+                for (i in 0 until n)
+                {
+                    println(i+1)
+                    moves[i].Filling()
+                }
+                while (true) {
+                    println("1. Вывести информацию")
+                    println("2. Посчитать выручку с фильма")
+                    println("3. Закончить")
+                    val act = readln().toInt()
+                    when (act){
+                        1 -> {
+                            print("№ фильма: ")
+                            val move = readln().toInt()
+                            when{
+                                (move in 1..n) -> println(moves[move-1].Info())
+                                else -> println("Не найдем фильм с таким номером")
+                            }
+                        }
+                        2 -> {
+                            print("№ фильма: ")
+                            val move = readln().toInt()
+                            when{
+                                (move in 1..n) -> println(moves[move-1].Revenue())
+                                else -> println("Не найдем фильм с таким номером")
+                            }
+                        }
+                        3 -> break
+                        else -> println("Неизвестная команда")
+                    }
+                }
+                println("Успешно завершено")
+            }
+        }
     }
     catch (e: Exception)
     {
