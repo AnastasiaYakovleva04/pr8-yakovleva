5304ba44ae3f17268e0004ec9316f48228cbee20
AnastasiaYakovleva04 <nastasiakovleva@gmail.com>
2025-02-28 16:19:28 +0500

    v12.kt

--- v12.kt
+++ v12.kt
@@ -1,12 +1,12 @@
 fun main() {
     try {
         print("Сколько фильмов вы хотите внести: ")
-        val n = readln().toInt()
+        val n = readLine()!!.toInt()
         when
         {
             n <= 0 -> println("Неверно введено количество фильмов")
             else -> {
-                val moves = Array(n) { Cinema() }
+                val moves = Array(n) { Cinema() } //Массив объектов(фильмов)
                 for (i in 0 until n)
                 {
                     println(i+1)
@@ -15,12 +15,14 @@ fun main() {
                 while (true) {
                     println("1. Вывести информацию")
                     println("2. Посчитать выручку с фильма")
-                    println("3. Закончить")
-                    val act = readln().toInt()
+                    println("3. Список фильмов")
+                    println("4. Посчитать цену для покупки m биетов")
+                    println("5. Закончить")
+                    val act = readLine()!!.toInt()
                     when (act){
                         1 -> {
                             print("№ фильма: ")
-                            val move = readln().toInt()
+                            val move = readLine()!!.toInt()
                             when{
                                 (move in 1..n) -> println(moves[move-1].Info())
                                 else -> println("Не найдем фильм с таким номером")
@@ -28,13 +30,31 @@ fun main() {
                         }
                         2 -> {
                             print("№ фильма: ")
-                            val move = readln().toInt()
+                            val move = readLine()!!.toInt()
                             when{
                                 (move in 1..n) -> println(moves[move-1].Revenue())
                                 else -> println("Не найдем фильм с таким номером")
                             }
                         }
-                        3 -> break
+                        3 -> {
+                            println(moves[0].MoveList(moves))
+                        }
+                        4 -> {
+                            print("Введите номер фильма: ")
+                            val move = readLine()!!.toInt()
+                            when {
+                                (move in 1..n) ->{
+                                    print("Введите количество билетов: ")
+                                    val ticket = readLine()!!.toUInt()
+                                    when {
+                                        (ticket == 0u) -> println("Неверно введено количество билетов")
+                                        else -> println(moves[move-1].Pay(moves[move-1].price, ticket))
+                                    }
+                                }
+                                else -> println("Не найдем фильм с таким номером")
+                            }
+                        }
+                        5 -> break
                         else -> println("Неизвестная команда")
                     }
                 }
